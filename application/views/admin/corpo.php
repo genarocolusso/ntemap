9525f1da10e3f7d0b0505d6073e68f9e30514812
Genaro Colusso <genarocolusso@gmail.com>
2016-01-15 08:21:14 -0200

    mudanca no painel . layout

--- application/views/admin/corpo.php
+++ application/views/admin/corpo.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
     <div class="side-body padding-top">
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <a href="#">
                     <div class="card red summary-inline">
                         <div class="card-body">
@@ -17,7 +17,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <a href="#">
                     <div class="card yellow summary-inline">
                         <div class="card-body">
@@ -31,7 +31,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <a href="#">
                     <div class="card green summary-inline">
                         <div class="card-body">
@@ -52,20 +52,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-                <a href="#">
-                    <div class="card blue summary-inline">
-                        <div class="card-body">
-                            <i class="icon fa fa-share-alt fa-4x"></i>
-                            <div class="content">
-                                <div class="title">16</div>
-                                <div class="sub-title">Projetos</div>
-                            </div>
-                            <div class="clear-both"></div>
-                        </div>
-                    </div>
-                </a>
-            </div>
+             
         </div>
         <div class="row  no-margin-bottom">
             <div class="col-sm-6 col-xs-12">
--- application/views/admin/infopolos.php
+++ application/views/admin/infopolos.php
@@ -2,71 +2,7 @@
 <!-- Main Content -->
 <div class="container-fluid">
   <div class="side-body padding-top">
-    <div class="row">
-      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-        <a href="#">
-          <div class="card red summary-inline">
-            <div class="card-body">
-              <i class="icon fa fa-institution fa-4x"></i>
-              <div class="content">
-                <div class="title"><?= sizeof($polos) ?></div>
-                <div class="sub-title">Polos</div>
-              </div>
-              <div class="clear-both"></div>
-            </div>
-          </div>
-        </a>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-        <a href="#">
-          <div class="card yellow summary-inline">
-            <div class="card-body">
-              <i class="icon fa fa-book fa-4x"></i>
-              <div class="content">
-                <div class="title"><?= sizeof($cursos) ?></div>
-                <div class="sub-title">Cursos</div>
-              </div>
-              <div class="clear-both"></div>
-            </div>
-          </div>
-        </a>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-        <a href="#">
-          <div class="card green summary-inline">
-            <div class="card-body">
-              <i class="icon fa fa-users fa-4x"></i>
-              <div class="content">
-                <div class="title"><?php  
-                  $nalunos = 0;
-                  foreach ($cursos as $key => $value) {
-                    $nalunos += $value['numalunos'];
-                  } 
-
-                  echo $nalunos;
-                  ?></div>
-                  <div class="sub-title">Alunos</div>
-                </div>
-                <div class="clear-both"></div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
-          <a href="#">
-            <div class="card blue summary-inline">
-              <div class="card-body">
-                <i class="icon fa fa-share-alt fa-4x"></i>
-                <div class="content">
-                  <div class="title">16</div>
-                  <div class="sub-title">Projetos</div>
-                </div>
-                <div class="clear-both"></div>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
+    
       <div class="row  no-margin-bottom">
         <div class="col-sm-6 col-xs-12">
           <div class="row">
